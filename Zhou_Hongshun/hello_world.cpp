e537ee8230b196b6c739868d9acd20408e03fd08
Zhou_Hongshun <mevozhbunalr@gmail.com>
2022-09-19 00:38:04 +0300

    Add hello_world app

---
+++ Zhou_Hongshun/hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Zhou Hongshun" << std::endl;
+  std::cout << "ЧЖоу Хуншунь" << std::endl;
+  return 0;
+}
