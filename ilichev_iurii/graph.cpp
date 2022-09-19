afc2f730fbfacca2cc4a0a74b6ed3ea20de5fb49
Ilichev IUrii <formalakk@gmail.com>
2022-09-19 19:53:07 +0300

    Code reorganization, error correction, adding comments

--- ilichev_iurii/graph.cpp
+++ ilichev_iurii/graph.cpp
@@ -1,78 +1,89 @@
 #include "graph.hpp"
 
-struct Graph::Edge {
- public:
-  Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id)
-      : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
-
-  EdgeId id() const { return id_; }
-  VertexId from_vertex_id() const { return from_vertex_id_; }
-  VertexId to_vertex_id() const { return to_vertex_id_; }
-
-  bool operator<(const Edge& edge) const { return edge.id_ > id_; }
-
- private:
-  EdgeId id_ = 0;
-  VertexId from_vertex_id_ = 0;
-  VertexId to_vertex_id_ = 0;
-};
-
-struct Graph::Vertex {
- public:
-  explicit Vertex(VertexId id) : id_(id) {}
-  explicit Vertex(const Vertex& vertex) : id_(vertex.id_) {}
-
-  VertexId id() const { return id_; };
-
-  bool operator<(const Vertex& vertex) const { return vertex.id_ > id_; };
-
- private:
-  VertexId id_;
-};
+#include <cassert>
+#include <iostream>
 
 void Graph::add_vertex() {
-  vertex_map_.insert(
-      std::make_pair(Vertex(vertex_map_.size()), std::set<Edge>()));
+  const auto next_vertex_id = VertexId(vertexes_.size());
+
+  vertexes_.insert({next_vertex_id, Vertex(next_vertex_id)});
+  vertex_edges_.insert({next_vertex_id, {}});
 }
 
 void Graph::add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
-  if (vertex_map_.find(Vertex(from_vertex_id)) == vertex_map_.end() ||
-      vertex_map_.find(Vertex(to_vertex_id)) == vertex_map_.end()) {
+  // Checking that an edge can be added between these vertices
+  assert(edge_correctness(from_vertex_id, to_vertex_id));
+
+  const auto next_edge_id = EdgeId(edges_.size());
+
+  edges_.insert(
+      {next_edge_id, Edge(next_edge_id, from_vertex_id, to_vertex_id)});
+  vertex_edges_[from_vertex_id].push_back(next_edge_id);
+  vertex_edges_[to_vertex_id].push_back(next_edge_id);
+}
+
+// Checking that an edge can be added between the vertices
+bool Graph::edge_correctness(VertexId from_vertex_id, VertexId to_vertex_id) {
+  // Checking that vertices exist
+  bool has_from_vertex = vertexes_.find(from_vertex_id) != vertexes_.end();
+  bool has_to_vertex = vertexes_.find(to_vertex_id) != vertexes_.end();
+  if (!has_from_vertex || !has_to_vertex) {
+    return false;
+  }
+
+  // Checking that there is only one edge between two vertices
+  // (includes the case of one loop for a vertex)
+  for (auto edge_id : vertex_edges_[from_vertex_id]) {
+    if (edges_[edge_id].from_vertex_id() == to_vertex_id ||
+        edges_[edge_id].to_vertex_id() == to_vertex_id) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Prints a list of the IDs of vertices and edges
+// that are associated with this vertex
+void Graph::print_graph_info() {
+  if (!vertex_edges_.size()) {
+    std::cout << "empty graph" << std::endl;
     return;
   }
 
-  auto edge = Edge(next_edge_id_++, from_vertex_id, to_vertex_id);
-  vertex_map_[Vertex(from_vertex_id)].insert(edge);
-  vertex_map_[Vertex(to_vertex_id)].insert(edge);
-}
+  for (auto vertex : vertex_edges_) {
+    std::cout << vertex.first << ": ";
 
-const int kVerticesCount = 13;
-
-int main() {
-  auto graph = Graph();
-
-  for (int i = 0; i < kVerticesCount; i++) {
-    graph.add_vertex();
+    if (!vertex.second.size()) {
+      std::cout << "vertex has no edges" << std::endl;
+    } else {
+      for (auto edge_id : vertex.second) {
+        std::cout << edge_id << " ";
+      }
+      std::cout << std::endl;
+    }
   }
-
-  graph.add_edge(0, 1);
-  graph.add_edge(0, 2);
-  graph.add_edge(0, 3);
-  graph.add_edge(1, 4);
-  graph.add_edge(1, 5);
-  graph.add_edge(1, 6);
-  graph.add_edge(2, 7);
-  graph.add_edge(2, 8);
-  graph.add_edge(3, 9);
-  graph.add_edge(4, 10);
-  graph.add_edge(5, 10);
-  graph.add_edge(6, 10);
-  graph.add_edge(7, 11);
-  graph.add_edge(8, 11);
-  graph.add_edge(9, 12);
-  graph.add_edge(10, 13);
-  graph.add_edge(11, 13);
-  graph.add_edge(12, 13);
-
-  return 0;
 }
+
+Graph::Edge::Edge() {}
+
+Graph::Edge::Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id)
+    : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
+
+Graph::EdgeId Graph::Edge::id() const {
+  return id_;
+}
+
+Graph::VertexId Graph::Edge::from_vertex_id() const {
+  return from_vertex_id_;
+}
+
+Graph::VertexId Graph::Edge::to_vertex_id() const {
+  return to_vertex_id_;
+}
+
+Graph::Vertex::Vertex(Graph::VertexId id) : id_(id) {}
+
+Graph::VertexId Graph::Vertex::id() const {
+  return id_;
+};
--- ilichev_iurii/graph.hpp
+++ ilichev_iurii/graph.hpp
@@ -1,19 +1,51 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <list>
 #include <map>
 #include <set>
 #include <utility>
 
+// An undirected graph, there can be only one edge between any two vertices
 class Graph {
  public:
   using VertexId = int;
   using EdgeId = int;
 
-  struct Vertex;
-  struct Edge;
+  struct Edge {
+   public:
+    Edge();
+    Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id);
+
+    EdgeId id() const;
+    VertexId from_vertex_id() const;
+    VertexId to_vertex_id() const;
+
+   private:
+    EdgeId id_;
+    VertexId from_vertex_id_;
+    VertexId to_vertex_id_;
+  };
+
+  struct Vertex {
+   public:
+    explicit Vertex(VertexId id);
+
+    VertexId id() const;
+
+   private:
+    VertexId id_;
+  };
 
   void add_vertex();
   void add_edge(VertexId from_vertex_id, VertexId to_vertex_id);
+  void print_graph_info();
+  bool edge_correctness(VertexId from_vertex_id, VertexId to_vertex_id);
 
  private:
-  std::map<Vertex, std::set<Edge>> vertex_map_;
-  EdgeId next_edge_id_ = 0;
+  std::map<VertexId, Vertex> vertexes_;
+  std::map<EdgeId, Edge> edges_;
+  std::map<VertexId, std::list<EdgeId>> vertex_edges_;
 };
+
+#endif
