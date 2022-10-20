9cb573a26f44671dc71ae7f5ef084e66c4f182dd
Ludmik <slavakr01@mail.ru>
2022-10-20 12:56:34 +0300

    graph generator added for Task02

--- krymskiy_sviatoslav/graph_generator.cpp
+++ krymskiy_sviatoslav/graph_generator.cpp
@@ -1,58 +1,63 @@
 #include <iostream>
 #include <vector>
 
-class Graph {
-    using VertexId = int;
-    using EdgeId = int;
+#include "graph_generator.hpp"
+
+struct Graph::Vertex {
 public:
-    struct Vertex {
-    public:
-        explicit Vertex(VertexId id) : id_(id) {}
-        VertexId id() const { return id_; }
-
-    private:
-        VertexId id_ = 0;
-    };
-
-    struct Edge {
-    public:
-        Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id) :
-                id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
-
-        EdgeId id() const { return id_; }
-        VertexId from_vertex_id() const { return from_vertex_id_; }
-        VertexId to_vertex_id() const { return to_vertex_id_; }
-
-    private:
-        EdgeId id_ = 0;
-        VertexId from_vertex_id_ = 0;
-        VertexId to_vertex_id_ = 0;
-    };
-    void add_vertex() {
-        verticesAmount++;
-    }
-    void add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
-        if (from_vertex_id >= verticesAmount || to_vertex_id >= verticesAmount) {
-            std::cout << "Wrong vertexes!\n";
-            return;
-        }
-        int graphSize = int(graphEdges.size());
-        Edge edge(graphSize, from_vertex_id, to_vertex_id);
-        graphEdges.push_back(edge);
-    }
+    Vertex(VertexId id) : id_(id) {}
+    VertexId id() const { return id_; }
 
 private:
-    int verticesAmount = 0;
-    std::vector<Edge> graphEdges;
+    VertexId id_ = 0;
 };
 
-int main() {
+struct Graph::Edge {
+public:
+    Edge(EdgeId id, VertexId from_vertex_id, VertexId to_vertex_id)
+            : id_(id), from_vertex_id_(from_vertex_id), to_vertex_id_(to_vertex_id) {}
+
+    EdgeId id() const { return id_; }
+    VertexId from_vertex_id() const { return from_vertex_id_; }
+    VertexId to_vertex_id() const { return to_vertex_id_; }
+
+private:
+    EdgeId id_ = 0;
+    VertexId from_vertex_id_ = 0;
+    VertexId to_vertex_id_ = 0;
+};
+
+void Graph::add_vertex() {
+    int verticesAmount = int(graphVertices.size());
+    Vertex vertex(verticesAmount);
+    graphVertices.push_back(vertex);
+}
+
+const std::vector<Graph::Vertex>& Graph::get_vertices() const {
+    return graphVertices;
+}
+
+const std::vector<Graph::Edge>& Graph::get_edges() const {
+    return graphEdges;
+}
+
+void Graph::add_edge(VertexId from_vertex_id, VertexId to_vertex_id) {
+    int verticesAmount = int(graphVertices.size());
+    if (from_vertex_id >= verticesAmount || to_vertex_id >= verticesAmount) {
+        std::cout << "Wrong vertexes!\n";
+        return;
+    }
+    int graphSize = int(graphEdges.size());
+    Edge edge(graphSize, from_vertex_id, to_vertex_id);
+    graphEdges.push_back(edge);
+}
+
+Graph generate_graph() {
     auto graph = Graph();
     int kVerticesCount = 14;
     for (int i = 0; i < kVerticesCount; i++) {
         graph.add_vertex();
     }
-
     graph.add_edge(0, 1);
     graph.add_edge(0, 2);
     graph.add_edge(0, 3);
@@ -72,5 +77,10 @@ int main() {
     graph.add_edge(11, 13);
     graph.add_edge(12, 13);
 
+    return graph;
+}
+
+int main() {
+    const auto graph = generate_graph();
     return 0;
 }
