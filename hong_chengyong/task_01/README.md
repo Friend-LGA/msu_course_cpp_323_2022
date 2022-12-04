46123300ef5bfc70674f0039b1d0d5312c703683
ChengyongHong <Hcy1356214049@gmail.com>
2022-12-04 20:41:22 +0800

    re_upload
    re_upload

---
+++ hong_chengyong/task_01/README.md
@@ -0,0 +1,1 @@
+Chengyong_Hong
---
+++ hong_chengyong/task_01/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+<<<<<<< HEAD
+  std::cout << "Hello World!\n"
+            << "Chengyong_Hong" << std::endl;
+=======
+  std::cout << "Hello World!\n"<<"Chengyong_Hong" << std::endl;
+>>>>>>> fb99aae48ef1e53cb768caaf7dacdbb80ab41d7e
+  return 0;
+}
