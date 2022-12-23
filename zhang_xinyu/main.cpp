987d604de4ad3ecb7b135112dd7948abbef2568c
YamyuCheung <s02190797@gse.cs.msu.ru>
2022-12-23 11:17:57 +0300

    chg

--- zhang_xinyu/main.cpp
+++ zhang_xinyu/main.cpp
@@ -7,8 +7,8 @@
 #include "graph_generation_controller.hpp"
 #include "graph_generator.hpp"
 #include "graph_json_printing.hpp"
-#include "graph_logger.hpp"
 #include "graph_printing.hpp"
+#include "logger.hpp"
 
 namespace fs = std::filesystem;
 
@@ -92,8 +92,8 @@ int handle_threads_count_input() {
 }
 
 void prepare_temp_directory() {
-  if (!fs::exists(config::kTempDirectoryPath)) {
-    if (!fs::create_directories(config::kTempDirectoryPath)) {
+  if (!fs::exists(uni_course_cpp::config::kTempDirectoryPath)) {
+    if (!fs::create_directories(uni_course_cpp::config::kTempDirectoryPath)) {
       throw std::runtime_error("Temp directory error!");
     }
   }
@@ -119,9 +119,9 @@ std::vector<uni_course_cpp::Graph> generate_graphs(
         const auto graph_description = printing::print_graph(graph);
         logger.log(generation_finished_string(index, graph_description));
         const auto graph_json = printing::json::print_graph(graph);
-        write_to_file(graph_json, std::string(config::kTempDirectoryPath) +
-                                      "graph_" + std::to_string(index) +
-                                      ".json");
+        write_to_file(graph_json,
+                      std::string(uni_course_cpp::config::kTempDirectoryPath) +
+                          "graph_" + std::to_string(index) + ".json");
       });
 
   return graphs;
